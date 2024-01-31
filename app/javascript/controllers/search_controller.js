a4f96b881211274c22259a95ffc5b463f89d4011
franci-lobbie lalane <francilobbie.lalane@gmail.com>
2024-01-31 09:28:45 +0100

    can focus search input by pressing keyboard

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -3,7 +3,34 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="search"
 export default class extends Controller {
   static targets = [ "input" ]
+
   connect() {
-    console.log("Hello, Stimulus!", this.element)
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+    document.addEventListener('keydown', this.handleKeyDown, true);
   }
+
+  disconnect() {
+    document.removeEventListener('keydown', this.handleKeyDown, true);
+  }
+
+  handleKeyDown(event) {
+    if (event.ctrlKey && event.key.toLowerCase() === 'k') {
+      event.preventDefault();
+      this.focus();
+      console.log("Ctrl + K");
+    } else if (event.key === 'Escape') {
+      event.preventDefault();
+      console.log("Esc");
+      this.blur();
+    }
+  }
+
+  focus() {
+    this.inputTarget.focus();
+  }
+
+  blur() {
+    this.inputTarget.blur();
+  }
+
 }
