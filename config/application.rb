4ca380ebac462e96ca29ab7e5ebf3ebf5aa180c5
franci-lobbie lalane <francilobbie.lalane@gmail.com>
2024-11-19 19:00:45 +0100

    creating my dockerfile image

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,8 @@ module Rubyist
     # config.eager_load_paths << Rails.root.join("extras")
     #
     config.active_job.queue_adapter = :sidekiq
+    config.i18n.default_locale = :fr
+
 
   end
 end
